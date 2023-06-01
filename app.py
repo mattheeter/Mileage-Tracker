0af8ea2b62d472d5d83533334a0e848e1d5c82d1
Matthew Heeter <matthewheeter1611@gmail.com>
2023-05-31 20:30:52 -0400

    Basic app returning a string

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask # Importing the Flask class to create a Flask object
+from flask import render_template # render_template used to return html from the routes
+
+app = Flask(__name__) # Creating new Flask object, with the special vairable __name__ used to automatically choose the module name
+
+@app.route('/') # Decorator for "registering" the route
+def home():
+    return "Hello World!"
